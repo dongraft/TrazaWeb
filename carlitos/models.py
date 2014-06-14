d187996b2cf4c0f8468c5a001eb9ddcff1ad820d
Jaime Carrera <dongraft@gmail.com>
2014-06-14 01:22:00 -0400

    charfield mejor que textfield

--- carlitos/models.py
+++ carlitos/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 
 class Traza(models.Model):
-    host = models.TextField()
-    ip = models.TextField()
-    lat = models.TextField()
-    lng = models.TextField()
-    accu = models.TextField()
-    p_transmitted = models.TextField()
-    p_received = models.TextField()
-    time = models.TextField()
-    min_t = models.TextField()
-    avg_t = models.TextField()
-    max_t = models.TextField()
-    stdev = models.TextField()
+    host = models.CharField(max_length=64)
+    ip = models.CharField(max_length=64)
+    lat = models.CharField(max_length=64)
+    lng = models.CharField(max_length=64)
+    accu = models.CharField(max_length=64)
+    p_transmitted = models.CharField(max_length=64)
+    p_received = models.CharField(max_length=64)
+    time = models.CharField(max_length=64)
+    min_t = models.CharField(max_length=64)
+    avg_t = models.CharField(max_length=64)
+    max_t = models.CharField(max_length=64)
+    stdev = models.CharField(max_length=64)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
