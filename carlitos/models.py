61d34c0496527fe757bc1a1d96b8bbcab6580d4c
Jaime Carrera <dongraft@gmail.com>
2014-06-16 21:17:52 -0400

    arreglando lso cambios de luis

--- carlitos/models.py
+++ carlitos/models.py
@@ -2,18 +2,19 @@ from django.db import models
 
 class Traza(models.Model):
     mobile_id = models.CharField(max_length=16)
+    timestamp = models.CharField(max_length=64)
     host = models.CharField(max_length=64)
-    ip = models.CharField(max_length=64)
-    lat = models.CharField(max_length=64)
-    lng = models.CharField(max_length=64)
-    accu = models.CharField(max_length=64)
     p_transmitted = models.CharField(max_length=64)
     p_received = models.CharField(max_length=64)
+    p_lost = models.CharField(max_length=64)
     time = models.CharField(max_length=64)
     min_t = models.CharField(max_length=64)
     avg_t = models.CharField(max_length=64)
     max_t = models.CharField(max_length=64)
     stdev = models.CharField(max_length=64)
+    lat = models.CharField(max_length=64)
+    lng = models.CharField(max_length=64)
+    accu = models.CharField(max_length=64)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
