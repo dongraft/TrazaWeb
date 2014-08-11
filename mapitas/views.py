0c38a293e86e882a0d536ca78296008e55b54e74
Jaime Carrera <dongraft@gmail.com>
2014-08-11 17:50:27 -0400

    agrego filtros de mentira

--- mapitas/views.py
+++ mapitas/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import json
 import locale
 from django.http import HttpResponse, HttpResponseNotAllowed, HttpResponseForbidden
@@ -10,7 +11,31 @@ from carlitos.models import Traza
 
 # Create your views here.
 def stats(request):
-    return render(request, 'mapitas/index.html', {'request':request})
+    if request.method == 'GET':
+        companies = {
+            'Claro': '0\n',
+            'Entel': '1\n',
+            'Movistar': '2\n',
+            'Nextel': '3\n',
+            'Virgin Mobile': '4\n',
+            'VTR': '5\n',
+        }
+        networktypes = {
+            'EDGE': '1', '3G': '2', '3.5G': '3', '4G': '3',
+        }
+        times = {
+            'Mañana (6am a 2pm)': '6',
+            'Tarde (2pm a 10pm)': '14',
+            'Noche (10pm a 6am)': '22',
+        }
+        data = {
+            'companies': companies,
+            'networktypes': networktypes,
+            'times': times,
+        }
+        return render(request, 'mapitas/index.html', data)
+    else:
+        return HttpResponseNotAllowed(['GET'])
 
 
 def get_data(request):
